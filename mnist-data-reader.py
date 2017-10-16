b3605d1b86cc45d052fc4efba3e3dce6ba7af599
Dillon <Dillonward13@gmail.com>
2017-10-16 20:52:30 +0100

    reading in images from data file and storing bytes

--- mnist-data-reader.py
+++ mnist-data-reader.py
@@ -1,4 +1,4 @@
-# reads data from files byte by byte
+# decompresses and reads files byte by byte
 # Author: Dillon Ward (Dillonward2017@gmail.com)
 # Date: 16/10/2017
 
@@ -6,19 +6,41 @@
 import gzip
 import numpy as np
 
+# path to the images
+train_images = 'data/train-images-idx3-ubyte.gz'
+test_images = 'data/t10k-images-idx3-ubyte.gz'
+
+# Part 1
 def read_images(filename):
     with gzip.open(filename, 'rb') as f:
+        # reads in the first byte which is the magic number
+            # prints out the magic number - new way of formatting
         magic = int.from_bytes(f.read(4), 'big')
-        # prints out the magic number - new way of formatting
         print("The Magic number is: {:>6}".format(magic))
 
+        # reads in the next byte which is the number of images
         imgs = int.from_bytes(f.read(4), 'big')
         print('Number of images: {:>10}'.format(imgs))
 
+        # reads in the number of rows and columns
         rows, cols = int.from_bytes(f.read(4), 'big'), int.from_bytes(f.read(4), 'big')
         print('Rows:{:>20}'.format(rows))
         print('Cols: {:>19}'.format(cols))
 
+        # list of images
         images = []
 
-train_images = read_images('data/train-images-idx3-ubyte.gz')
+        # loop for the amount of images
+        for i in range(imgs):
+            no_rows = []
+            for r in range(rows):
+                no_cols = []
+                for c in range(cols):
+                    no_cols.append(int.from_bytes(f.read(1), 'big'))
+                no_rows.append(cols)
+            images.append(rows)
+    
+    return images
+
+read_images(train_images)
+read_images(test_images)
